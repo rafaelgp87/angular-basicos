a7d31e12d5f6bb48df378a8413ec5aa2ae378d16
Rafael Gutiérrez <rafaelgp87@gmail.com>
2021-03-17 21:06:49 -0600

    Primer commit.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { HeroeModule } from './heroes/heroes.module';
+import { ContadorModule } from './contador/contador.module';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroeModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
